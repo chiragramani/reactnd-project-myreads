d14c4f12908ce1cf8ae519cd817c79275b6ba000
Chirag Ramani <chirag.ramani7@gmail.com>
2019-03-03 15:59:21 +0530

    Abstracted into OpenSearch and ListBooks component

---
+++ src/OpenSearch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function OpenSearch(props) {
+  return (
+    <div className="open-search">
+      <button>
+        Add a book
+      </button>
+    </div>
+  );
+}
+
+export default OpenSearch;
